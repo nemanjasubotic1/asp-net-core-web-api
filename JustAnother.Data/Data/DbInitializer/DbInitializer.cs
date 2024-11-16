d1c36181339ab4b867be3b128f779b5967089310
nemanjasubotic1 <103412864+nemanjasubotic1@users.noreply.github.com>
2024-11-16 10:55:57 +0100

    Create dummy admin on first application run

--- JustAnother.Data/Data/DbInitializer/DbInitializer.cs
+++ JustAnother.Data/Data/DbInitializer/DbInitializer.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using JustAnother.Model.Entity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace JustAnother.DataAccess.Data.DbInitializer;
@@ -8,10 +9,13 @@ public class DbInitializer : IDbInitializer
     private readonly ApplicationDbContext _db;
 
     private readonly RoleManager<IdentityRole> _roleManager;
-    public DbInitializer(ApplicationDbContext db, RoleManager<IdentityRole> roleManager)
+
+    private readonly UserManager<ApplicationUser> _userManager;
+    public DbInitializer(ApplicationDbContext db, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
     {
         _db = db;
         _roleManager = roleManager;
+        _userManager = userManager;
     }
 
     public async void Initialize()
@@ -23,8 +27,26 @@ public class DbInitializer : IDbInitializer
 
         if (!_roleManager.RoleExistsAsync(StaticDetails.Role_Admin).GetAwaiter().GetResult())
         {
-             _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_Admin)).Wait();
-             _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_User)).Wait();
+            _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_Admin)).Wait();
+            _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_User)).Wait();
+
+            ApplicationUser user = new()
+            {
+                Name = "Main Admin",
+                UserName = "admin@gmail.com",
+                NormalizedUserName = "ADMIN@GMAIL.COM",
+                Email = "admin@gmail.com",
+                NormalizedEmail = "ADMIN@GMAIL.COM",
+                PhoneNumber = "1234567890",
+            };
+
+            var result = _userManager.CreateAsync(user, "admin123").GetAwaiter().GetResult();
+
+            if (result.Succeeded)
+            {
+                ApplicationUser userFromDb = await _db.ApplicationUsers.FirstOrDefaultAsync(l => l.UserName == "admin@gmail.com");
+                await _userManager.AddToRoleAsync(userFromDb, StaticDetails.Role_Admin);
+            }
         }
     }
 }
